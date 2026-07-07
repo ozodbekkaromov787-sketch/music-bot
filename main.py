e6eff1cf4490c1ff9e1dabaf2d358869e65e5eb5
ozodbekkaromov787-sketch <ozodbekkaromov787@gmail.com>
2026-07-07 13:29:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-# Tokenni Render muhitidan (Environment Variables) oladi
 TOKEN = os.environ.get("TOKEN")
 
 async def start(update, context):
@@ -11,5 +10,6 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+    
     
   
