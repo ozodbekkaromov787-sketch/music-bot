e9bdd5d6e6e0ab6737fdc6cb80273db54371238b
ozodbekkaromov787-sketch <ozodbekkaromov787@gmail.com>
2026-07-07 13:17:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# Tokenni Render muhitidan oladi
+TOKEN = os.environ.get("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Salom! Bot ishlamoqda!')
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+  
